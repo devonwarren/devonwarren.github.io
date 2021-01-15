f8a79f5d85758e8695c2655d138fba2a64734175
Devon Warren <devon.warren@gmail.com>
2021-01-15 16:24:44 -0500

    initial blog start

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Devon Warren'
+SITENAME = 'Devon Warren'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+OUTPUT_PATH = 'docs/'
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         )
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/devonwarren'),
+          ('LinkedIn', 'https://www.linkedin.com/in/devonwarren/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# customizations
+THEME = 'pelican-blue'
+
+SIDEBAR_DIGEST = 'Developer, Linux Admin, Tech Enthusiast'
